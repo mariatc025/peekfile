c8083ab099705869b5d4cbc1ef1a5098fbdce828
Maria <mariatc025@gmail.com>
2024-10-31 00:46:08 +0100

    fourth commit

--- peek.sh
+++ peek.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
 if [[ -n $2 ]];
 then NLINES=$2;
 else NLINES=3;
 fi;
 
-head -n $NLINES "$1";
-echo ...;
-tail -n $NLINES "$1";
+FLINES=$(cat $1 | wc -l)
+if [[ $FLINES -le 2*$NLINES ]];
+	then cat $1;
+else
+	echo Warning: not all lines are being displayed;
+	head -n $NLINES "$1";
+	echo ...;
+	tail -n $NLINES "$1";
+fi;
