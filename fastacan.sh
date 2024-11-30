318ee9d39f97fdf5c4cdc97ffc24c2cf17cf69f4
Maria <mariatc025@gmail.com>
2024-12-01 00:04:06 +0100

    second commit

--- fastacan.sh
+++ fastacan.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-
-
-
 ## Argument 1 check:
  
 if [[ -z $1 ]]  # check if $1 is empty
@@ -27,14 +24,21 @@ else
 	echo "Error!! Argument 2 is not numeric"
 fi
 
+
 ## Report
-FILES=$(find $folder -name "*.fasta" -or -name "*.fa" )
-NFILES=$(echo $FILES| wc -w)
+FILES=$(find $folder -name "*.fasta" -or -name "*.fa" ) # finds fasta or fa files in $folder and stores them in $FILES (in a single line)
+NFILES=$(echo $FILES| wc -w) # couts how many files it has found
+
 if [[ $NFILES -gt 0 ]]
-then 
-	echo "There are $NFILES fasta or fa files in $folder."  ## 1 file?
+then
+	if [[ $NFLIES -eq 1 ]]
+	then 
+		echo "There is $NFILES fasta or fa file in $folder."
+	else
+		echo "There are $NFILES fasta or fa files in $folder."
+	fi
 	
-	IDtotal=$(awk '/^>/{print $1}' $FILES| sort | uniq -c | wc -l)
+	IDtotal=$(awk '/^>/{print $1}' $FILES | sort | uniq -c | wc -l)
 	echo "There are $IDtotal unique fasta IDs"
 	
 	for i in $FILES
@@ -46,9 +50,31 @@ then
 		else
 			echo "Symlink: No"
 		fi
-		echo "Number of sequences: $(awk '/^>/{print $1}' $i| wc -l)"
-		echo "Total sequence length: $(awk '!/>/{gsub(/-/, "", $0);gsub(" ", "", $0); total_length += length($0)} END {print total_length}' $i)"
-		echo	
+		NSEQ=$(awk '/^>/{print $1}' $i| wc -l)
+		echo "Number of sequences: $NSEQ"
+		
+		TSEQL=$(awk '!/>/{gsub(/-/, "", $0); gsub(" ", "", $0); total_length += length($0)} END {print total_length}' $i) 
+		echo "Total sequence length: $TSEQL"
+		
+		ACTG_counts=$(awk '!/>/{gsub(/-/, "", $0); print $0}' $i | grep -o '[actgACTG]' | wc -l)
+		total_counts=$(awk '!/>/{gsub(/-/, "", $0); print $0}' $i | grep -o '[^ ]' | wc -l)
+		if [[ ACTG_counts -gt 0 ]]
+		then
+			ACTG_freq=$(( ($ACTG_counts*100) / $total_counts )) # we multiply first because this expansion is integer only and changing the order we would always get 0
+			if [[ $ACTG_freq -gt 70 ]] # it is not 100% because there is the possibility of other IUPAC characters such as N
+			then
+				echo "Sequences content: nucleotides"
+			else
+				echo "Sequences content: amino acids"
+			fi
+		else
+			echo "Sequences content: amino acids"
+		fi
+		#if [[ $ACTG_freq -gt 70 ]]
+		#then
+		echo
+		 
+		
 	done
 	
 	
