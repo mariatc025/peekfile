3b27db7773db57f1443c30bcc1f5e24959f3c63b
Maria <mariatc025@gmail.com>
2024-12-01 13:07:38 +0100

    sixth commit

--- fastacan.sh
+++ fastacan.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-## Argument 1 check:
+## Argument 1 check
 if [[ -z $1 ]]  # check if $1 is empty
 then
 	folder=. # if it is empty set current folder as default
@@ -12,7 +12,7 @@ else
 	exit 1     # else print the error message and exit the program
 fi
 
-## Argument 2 check:
+## Argument 2 check
 if [[ -z $2 ]]  # check if $2 is empty
 then
 	N=0  # if it is empty set number of lines to 0
@@ -26,13 +26,17 @@ fi
 
 
 ## REPORT
-FILES=$(find $folder -name "*.fasta" -or -name "*.fa" ) # finds fasta or fa files in folder and subfolders and stores them in $FILES (in a single line)
-N_FILES=$(echo $FILES| wc -w) # couts how many files it has found
+FILES=$(find $folder -name "*.fasta" -or -name "*.fa" ) # finds fasta or fa files in folder and sub-folders,
+							# and stores them in $FILES (in a single line)
+N_FILES=$(echo $FILES| wc -w) # counts how many files it has found
 
 if [[ $folder == . ]] # if $folder is a . 
 then folder="the current folder" # changes it to "the current folder" improving readability in the report
 fi
 
+echo # new_line
+echo "###########  FASTA/FA files in $folder REPORT  ###########" # title of the report
+echo # new_line
 
 if [[ $N_FILES -gt 0 ]] # checks if at least 1 fa or fasta file was found
 then
@@ -43,38 +47,49 @@ then
 	fi
 	
 	## How many unique IDs there are
-	ID_total=$(awk '/^>/{print $1}' $FILES | sort | uniq -c | wc -l) # finds the lines beggining with > prints the id, sorts them and counts how many unique IDs there are
-	echo "There are $ID_total unique fasta IDs."
+	ID_total=$(awk '/^>/{print $1}' $FILES | sort | uniq -c | wc -l) # finds the lines beginning with > prints the id, 
+									 # sorts them and counts how many unique IDs there are					
+	if [[ $ID_total -eq 1 ]]						 
+	then echo "There is $ID_total unique fasta ID." # not likely, but prints this when there is only one unique fasta ID
+	else echo "There are $ID_total unique fasta IDs."
+	fi
+	
+	echo # new_line
 	
 	## for each file
 	for i in $FILES
 	do 
 		# filename
-		echo "=========$i========="
+		echo "========= $i ========="
 		
 		# If the file is a symlink or not
 		if [[ -L "$i" ]] 
 		then
-			echo "Symlink: Yes"
+			echo "Type symlink: Yes"
 		else
-			echo "Symlink: No"
+			echo "Type symlink: No"
 		fi
 		
 		# Number of sequences per file
-		N_SEQ=$(awk '/^>/{print $1}' $i| wc -l) # prints the IDs of the file and counts them and stores it in $NSEQ
+		N_SEQ=$(awk '/^>/{print $1}' $i | wc -l) # prints the IDs of the file and counts them and stores it in $N_SEQ
 		echo "Number of sequences: $N_SEQ"
 		
 		# Total sequence length per file, without -," " and \n
-		T_SEQ_L=$(awk '!/>/{gsub(/-/, "", $0); gsub(" ", "", $0); total_length += length($0)} END {print total_length}' $i) # finds the lines with no >, replaces - and spaces, counts the length per line and adds it to total length, which is stored in $TSEQL
+		T_SEQ_L=$(awk '!/>/{gsub(/-/, "", $0); gsub(" ", "", $0); total_length += length($0)} END {print total_length}' $i) 
+		# finds the lines with no >, deletes - and spaces, counts the length per line and adds it to total length, which is stored in $T_SEQ_L
 		echo "Total sequence length: $T_SEQ_L"
 		
 		# Sequences content: nucleotide or amino acids
-		ACTG_counts=$(awk '!/>/{gsub(/-/, "", $0); print $0}' $i | grep -o '[actgACTG]' | wc -l) # finds the lines with no >, replaces -, and from those lines greps the letters actg (case insensitive) and counts how many there are
-		TOTAL_counts=$(awk '!/>/{gsub(/-/, "", $0); print $0}' $i | grep -o '[^ ]' | wc -l) # finds the lines with no >, replaces -, and from those lines greps all the characters except spaces and counts how many there are
-		ACTG_freq=$(( ($ACTG_counts*100) / $TOTAL_counts )) # calculates the frequency of ACTG in the sequence
-								    # multiplies first because this expansion is integer only and changing the order we would always get 0
-		if [[ $ACTG_freq -gt 70 ]] # if the frequency is greater than 70 they are nucleotides. Protein sequences usually have ACTG with a frequency less than 50%
-					   # we don't put 100% because there is the possibility of other IUPAC characters such as N.
+		ACTG_counts=$(awk '!/>/{gsub(/-/, "", $0); print $0}' $i | grep -o '[actgACTG]' | wc -l) # finds the lines with no >, deletes -, and from those
+										# lines greps the letters actg (case insensitive) and counts how many there are
+		TOTAL_counts=$(awk '!/>/{gsub(/-/, "", $0); print $0}' $i | grep -o '[^ ]' | wc -l) # finds the lines with no >, deletes -, and from those
+										# lines greps all the characters except spaces and counts how many there are
+		
+		ACTG_freq=$(( ($ACTG_counts*100) / $TOTAL_counts )) # calculates the frequency of ACTG in the sequence,
+								    # multiplies first because this expansion is integer only and by changing the order we 
+								    # would always get 0
+		if [[ $ACTG_freq -gt 70 ]] # if the frequency is greater than 70 they are nucleotides. Protein sequences usually have ACTG with a frequency
+					   # less than 50%. We don't put 100% because there is the possibility of other IUPAC characters such as N.
 		then
 			echo "Sequences content: nucleotides"
 		else
@@ -82,7 +97,7 @@ then
 		fi
 		
 				
-		# Prints the file or the begining and ending, only if arg2 is not 0
+		# Prints the file or the beginning and ending, only if arg2 is not 0
 		FILE_lines=$(cat $i | wc -l) # counts number of lines in the file
 		if [[ $N != 0 ]] # only done if arg2 is not 0
 		then
@@ -94,13 +109,14 @@ then
 				head -n $N $i # otherwise it prints the first N lines and the last N lines
 				echo ...
 				tail -n $N $i
-			fi;
-		fi;
+			fi
+		fi
 		echo
 	done
 	
 else # no fa or fasta files found
 	echo "There are 0 fasta or fa files in $folder."
+	echo # new_line
 fi
 
 
